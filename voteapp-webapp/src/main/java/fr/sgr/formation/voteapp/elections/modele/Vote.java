4422f8e3dd7b2fea31abdc7d9ad37d3374316171
Françoise <francoise.goujard@laposte.net>
2016-12-16 06:25:16 -0500

    Création élection et vote

---
+++ voteapp-webapp/src/main/java/fr/sgr/formation/voteapp/elections/modele/Vote.java
@@ -0,0 +1,9 @@
+package fr.sgr.formation.voteapp.elections.modele;
+
+import fr.sgr.formation.voteapp.utilisateurs.modele.Utilisateur;
+
+public class Vote {
+
+	private Utilisateur utilisateur;
+	private Choix choix;
+}
