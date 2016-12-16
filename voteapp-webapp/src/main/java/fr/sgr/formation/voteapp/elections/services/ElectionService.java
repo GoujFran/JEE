11fccfc770d733b92d27b2bd6d4f400baae9123e
Françoise <francoise.goujard@laposte.net>
2016-12-16 06:46:52 -0500

    création fonction

--- voteapp-webapp/src/main/java/fr/sgr/formation/voteapp/elections/services/ElectionService.java
+++ voteapp-webapp/src/main/java/fr/sgr/formation/voteapp/elections/services/ElectionService.java
@@ -2,4 +2,23 @@ package fr.sgr.formation.voteapp.elections.services;
 
 public class ElectionService {
 
+	public void creerElection() {
+		// TODO
+	}
+
+	public void fermerElection() {
+		// TODO
+	}
+
+	public void modifierElection() {
+		// TODO
+	}
+
+	public void listerElection() {
+		// TODO
+	}
+
+	public void consulterRésultats() {
+		// TODO
+	}
 }
